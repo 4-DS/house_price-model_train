ea144604f486fdca048d945d55a2ce7bd20d59ed
ExcaliburKG <akagorbunov@ya.ru>
2023-09-05 10:29:12 +0300

    Added test data, changed pipeline name

--- model_service.py
+++ model_service.py
@@ -1,19 +1,26 @@
 from bentoml import env, artifacts, api, BentoService
 from bentoml.adapters import DataframeInput, JsonInput
 from bentoml.frameworks.sklearn import SklearnModelArtifact
-from bentoml.service.artifacts.common import TextFileArtifact
+from bentoml.service.artifacts.common import PickleArtifact, TextFileArtifact
 
 @env(infer_pip_packages=True)
 @artifacts([SklearnModelArtifact('model'),
+            PickleArtifact('test_data'),
             TextFileArtifact('service_version',
                              file_extension='.txt',
                              encoding='utf8')]) # for versions of bentoml 0.13 and newer   
 class ModelService(BentoService): 
     @api(input=DataframeInput(), batch=True)
     def predict(self, df):
-        return self.artifacts.model.predict(df.values) 
+        return self.artifacts.model.predict(df.values)
+    
+    @api(input=JsonInput(), batch=False)
+    def test_data(self, *args): 
+        """ Return some test data for running a test """
+        return self.artifacts.test_data
 
     @api(input=JsonInput(), batch=False)
     def service_version(self, *args): 
         """ Return version of a running service """
         return self.artifacts.service_version
+    
